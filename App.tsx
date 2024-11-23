181862218ca2c7aff95183cc715231b8b236e7b0
Diêgo de Barros <debarrosdiego415@gmail.com>
2024-11-23 11:09:17 -0300

    feat & update: feat mainTheme and update MyCalendar styles

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Calendar } from "react-native-calendars";
+import theme from "./themes/mainTheme";
 
 export default function App() {
   return (
@@ -8,19 +9,20 @@ export default function App() {
       <Text>Open up App.tsx to start working on your app!</Text>
       <Calendar
         style={{
-          borderWidth: 1,
+          borderWidth: 0,
           borderColor: "gray",
           height: 350,
+          borderRadius: 8,
         }}
         theme={{
-          backgroundColor: "#ffffff",
-          calendarBackground: "#ffffff",
-          textSectionTitleColor: "#b6c1cd",
-          selectedDayBackgroundColor: "#00adf5",
-          selectedDayTextColor: "#ffffff",
-          todayTextColor: "#00adf5",
-          dayTextColor: "#2d4150",
-          textDisabledColor: "#dd99ee",
+          backgroundColor: theme.colors.background,
+          calendarBackground: theme.colors.secondaryBackground,
+          textSectionTitleColor: theme.colors.primaryText,
+          selectedDayBackgroundColor: theme.colors.primary,
+          selectedDayTextColor: theme.colors.secondaryText,
+          todayTextColor: theme.colors.tertiary,
+          dayTextColor: theme.colors.secondaryText,
+          textDisabledColor: theme.colors.secondary,
         }}
       />
       <StatusBar style="auto" />
